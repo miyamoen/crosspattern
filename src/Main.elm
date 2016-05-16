58827067d3aa8e8977ad410ae41b012d415f09d8
miyamoen <akihito.biblioteca@gmail.com>
2016-05-17 08:15:27 +0900

    update elm-package.json for Elm 0.17.

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,8 @@ import SewingKit.Svg exposing (..)
 --import SewingKit.Color exposing (..)
 --import SewingKit.StitchList exposing (..)
 import SewingKit.Pattern exposing (..)
-
+--// "circuithub/elm-array-extra": "1.1.2 <= v < 2.0.0",
+--        // "tomjkidd/elm-multiway-tree-zipper": "1.1.0 <= v < 2.0.0"
 
 --main = start { model = (init (degrees 90) 1.0 0.5), update = update, view = view }
 --main = start { model = init 25 25, update = update, view = view }
